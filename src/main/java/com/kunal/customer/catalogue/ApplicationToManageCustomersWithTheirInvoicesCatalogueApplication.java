e284a0d00dac18d9722b24654f2658265abd688e
kkunal2022 <84966950+kkunal2022@users.noreply.github.com>
2023-09-23 14:54:36 +0300

    Initial Project Commit

---
+++ src/main/java/com/kunal/customer/catalogue/ApplicationToManageCustomersWithTheirInvoicesCatalogueApplication.java
@@ -0,0 +1,15 @@
+package com.kunal.customer.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+** Kumar.Kunal
+ */
+@SpringBootApplication
+public class ApplicationToManageCustomersWithTheirInvoicesCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationToManageCustomersWithTheirInvoicesCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/customer/catalogue/ApplicationToManageCustomersWithTheirInvoicesCatalogueApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kunal.customer.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+ ** Kumar.Kunal
+ */
+@SpringBootTest
+class ApplicationToManageCustomersWithTheirInvoicesCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
